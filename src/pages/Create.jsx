8b8cd7b275634aff1c7c4910a45ad53a3a21d1c6
Ebuka-Chucks <Chrisbrandford4@gmail.com>
2023-08-03 12:42:07 +0100

    first commit

---
+++ src/pages/Create.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import step from "../assets/amico.png";
+import {useNavigate} from "react-router-dom"
+ import { ToastContainer, toast } from "react-toastify";
+ import "react-toastify/dist/ReactToastify.css";
+  
+
+const Create = () => {
+  const redirect = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log({ title, description });
+    try {
+      const res = await fetch("https://goalonapiebuka.onrender.com/api/goals", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, description }),
+      });
+      const data = await res.json();
+      console.log(data);
+if (data.success) {
+  //show that it was successful
+   toast.success("Goal has been created");
+  //navigate to all goals page
+  redirect("/")
+} else {
+  //show error
+  toast.error("Failed to create Goal, Try Again..")
+}
+
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+     console.log(error); 
+    }
+  }
+  return (
+    <div className="container d-flex justify-content-between align-items-center mt-3 pb-3 gap-lg-2">
+      <div className="main-form py-5 px-1 ps-lg-2 ps-xl-3 pe-xl-3 rounded-2">
+        <ToastContainer />
+
+        <form className="create-form" onSubmit={handleSubmit}>
+          <div className="mt-2">
+            <input
+              type="text"
+              placeholder="Goal Title"
+              className="bg-transparent"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mt-5">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Goal Description"
+              className="bg-transparent"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
+          </div>
+          <div className="mt-2">
+            <button className="blue-bg p-2">Create Goal</button>
+          </div>
+        </form>
+      </div>
+      <div className="d-none d-lg-block main-img">
+        <img src={step} alt="image of a step" />
+      </div>
+    </div>
+  );
+};
+
+export default Create;
